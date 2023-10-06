cd106a4e3b9dfae7fcd2148222a684ab7d2a09b3
RudenkoOleksandr02 <gitemail02@gmail.com>
2023-10-06 20:17:40 +0300

    update furniture

--- src/api/furniture/controllers/furniture.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * furniture controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::furniture.furniture');
