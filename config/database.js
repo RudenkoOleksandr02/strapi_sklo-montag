0a4f2075479c6c6a65e5a102c4f99ad05f910071
RudenkoOleksandr02 <gitemail02@gmail.com>
2023-10-23 16:29:21 +0300

    fix error connect

--- config/database.js
+++ config/database.js
@@ -95,7 +95,7 @@ module.exports = ({ env }) => {
 const path = require('path');
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  /*const client = env('DATABASE_CLIENT', 'sqlite');
 
   const connections = {
     mysql: {
@@ -182,5 +182,34 @@ module.exports = ({ env }) => {
       ...connections[client],
       acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
     },
-  };
+  };*/
+
+  return {
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+        },
+      },
+      debug: false,
+      acquireConnectionTimeout: 600000,
+      pool: {
+        min: 0,
+        max: 100,
+        acquireTimeoutMillis: 300000,
+        createTimeoutMillis: 300000,
+        destroyTimeoutMillis: 50000,
+        idleTimeoutMillis: 300000,
+        reapIntervalMillis: 10000,
+        createRetryIntervalMillis: 2000,
+        propagateCreateError: false,
+      },
+    },
+  }
 };
